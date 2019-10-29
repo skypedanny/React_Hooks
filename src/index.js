239fcba7ce4371e53cba4519037866dd4863c614
DaeHun <skypedanny@naver.com>
2019-10-29 19:36:11 +0900

    #1.1 useInput
    {...name} name unpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
+
+//function에서 event 처리
+const App = () => {
+  const name = useInput("Mr.");
+  return (
+    //{...name} = name unpack
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
